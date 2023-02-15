ed7e00f86ef55b5962a5cf7542b0fa6a7b0ea81d
daniilsajhutdinov <jeffrixxxon@mail.ru>
2023-02-15 14:13:30 +0300

    Инициализация моделей жанров, языков и авторов книги.

--- WebBooks/WebBooks/urls.py
+++ WebBooks/WebBooks/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from catalog import views
 
+
 urlpatterns = [
     path('', views.index, name='home'),
     path('admin/', admin.site.urls),
--- WebBooks/catalog/models.py
+++ WebBooks/catalog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200,
+                            help_text='Введите жанр книги',
+                            verbose_name='Жанр книги')
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=20,
+                            help_text='Введите язык книги',
+                            verbose_name='Язык книги')
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100,
+                                  help_text='Введите имя автора',
+                                  verbose_name='Имя автора')
+    last_name = models.CharField(max_length=100,
+                                 help_text='Введите фамилию автора',
+                                 verbose_name='Фамилия автора')
+    date_of_brith = models.DateField(help_text='Введите дату рождения',
+                                     verbose_name='Дата рождения',)
+    date_of_death = models.DateField(help_text='Введите дату смерти',
+                                     verbose_name='Дата смерти',
+                                     null=True, blank=True)
+
+    def __str__(self):
+        return self.last_name
