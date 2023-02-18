d9f524cf72f74c5e7db6dacd9f791c15782b7e4c
daniilsajhutdinov <jeffrixxxon@mail.ru>
2023-02-18 18:12:30 +0300

    Работа с данными в Django Admin

--- WebBooks/catalog/admin.py
+++ WebBooks/catalog/admin.py
@@ -3,3 +3,9 @@ from .models import Author, Book, Genre, Language, Status, BookInstance
 
 # Register your models here.
 
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Status)
+admin.site.register(BookInstance)
