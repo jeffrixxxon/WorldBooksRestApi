7c22bdbb51b9b1006035afc732fceaa5bd795757
daniilsajhutdinov <jeffrixxxon@mail.ru>
2023-02-19 18:26:24 +0300

    Формирование базового шаблона с html разметкой. Формирование главнос страницы сайта.

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
-# Create your views here.
+
+class BookListView(generic.ListView):
+    model = Book
 
 
-def index(request, num_visits=None):
+def index(request):
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
     num_instances_availabel = BookInstance.objects.filter(status__exact=2).count()
@@ -13,5 +16,4 @@ def index(request, num_visits=None):
                                                   'num_instances': num_instances,
                                                   'num_instances_availabel': num_instances_availabel,
                                                   'num_authors': num_authors,
-                                                  'num_visits': num_visits,
                                                   })
