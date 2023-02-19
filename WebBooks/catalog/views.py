2f0e78ddc341afe88c960bfcabca477348ba80bd
daniilsajhutdinov <jeffrixxxon@mail.ru>
2023-02-19 12:37:38 +0300

    Создание файла urls, папку templates с html главной страницы сайта

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
 
 
-def index(request) -> HttpResponse:
-    return HttpResponse("Главая страница сайта Мир книг!")
+def index(request, num_visits=None):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_availabel = BookInstance.objects.filter(status__exact=2).count()
+    num_authors = Author.objects.count()
+    return render(request, 'index.html', context={'num_books': num_books,
+                                                  'num_instances': num_instances,
+                                                  'num_instances_availabel': num_instances_availabel,
+                                                  'num_authors': num_authors,
+                                                  'num_visits': num_visits,
+                                                  })
