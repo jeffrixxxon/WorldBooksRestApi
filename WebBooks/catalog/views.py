f265a786b5fbc19198da67a24af0b1f5038eb93e
daniilsajhutdinov <jeffrixxxon@mail.ru>
2023-02-22 19:34:21 +0300

    Добавление счетчика посещений страницы

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -22,8 +22,11 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     num_instances_availabel = BookInstance.objects.filter(status__exact=2).count()
     num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     return render(request, 'index.html', context={'num_books': num_books,
                                                   'num_instances': num_instances,
                                                   'num_instances_availabel': num_instances_availabel,
                                                   'num_authors': num_authors,
+                                                  'num_visits': num_visits
                                                   })
