fc101aa2176deafcb2059695b00aeacc44e262b8
daniilsajhutdinov <jeffrixxxon@mail.ru>
2023-02-22 18:31:26 +0300

    Реализация пагинации страниц в базовом шаблоне

--- WebBooks/WebBooks/urls.py
+++ WebBooks/WebBooks/urls.py
@@ -21,6 +21,7 @@ from catalog import views
 urlpatterns = [
     re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
     re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
